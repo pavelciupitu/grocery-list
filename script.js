aa5e145cae2812a9447bc2a87c82316b811a233e
Pavel Ciupitu <pavel.ciupitu@gmail.com>
2020-01-28 00:43:46 +0200

    Making the project to work in local storage & delete the whole list.

--- script.js
+++ script.js
@@ -1,28 +1,109 @@
 //variables
 
-const displayUp = document.querySelector('.displayAction');
-const insertText = document.querySelector('.text-input');
-const buttonAdd = document.querySelector('.buttonAdd');
+const addItemsAction = document.querySelector('.displayAction');
+const input = document.querySelector('.text-input');
+const submit = document.querySelector('.buttonAdd');
 
-const displayDown = document.querySelector('.displayItems');
-const theList = document.querySelector('.grocery-list');
-const clearAll = document.querySelector('.clearAll');
+const displayItemsAction = document.querySelector('.displayItems');
+const list = document.querySelector('.grocery-list');
+const clear = document.querySelector('.clearAll');
 
 
 // buttons - eventlistener
 
-buttonAdd.addEventListener('click', add);
+submit.addEventListener('click', addItem);
+document.addEventListener('DOMContentLoaded', displayStorage);
+clear.addEventListener('click', clearAll)
 
-
-clearAll.addEventListener('click', clear);
+//clear.addEventListener('click', clear);
 //functions
 
-function add() {
-let value = insertText.value;
-console.log(value);
+function addItem(event) {
+    event.preventDefault();
+    let value = input.value;
+    // console.log(value);
+    if(value === "") {
+        showAction(addItemsAction,'Please add grocery item', false);
+    } else {
+        showAction(addItemsAction, `${value} aded to the list`, true);
+        createItem(value);
+        updateStorage(value);
+    }
 }
 
-function clear(){
-let value = insertText.value;
-console.log(value);
+function showAction(element, text, value) {
+    if (value === true) {
+        element.classList.add('success');
+        element.innerText = text;
+        input.value = '';
+        setTimeout(function(){
+            element.classList.remove('success');
+        },4000 )
+    }
+    else {
+        element.classList.add('alert');
+        element.innerText = text;
+        input.value = '';
+        setTimeout(function(){
+            element.classList.remove('alert');
+        },4000 )
+    }
+}
+
+//!Create new item
+
+function createItem(value) {
+    let parent = document.createElement('div');
+    parent.classList.add('grocery-item');
+    parent.innerHTML = `                <h4 class="grocery-item__title">${value}</h4>
+    <a href="#" class="grocery-item__delete">
+        <i class="far fa-trash-alt"></i></a>`
+
+    list.appendChild(parent);
+}
+
+//!Update Storage
+
+function updateStorage(value) {
+    let groceryList;
+    let exists = localStorage.getItem('groceryList')
+
+    if(exists) {
+        groceryList = JSON.parse(localStorage.getItem('groceryList'))
+    }
+    else {
+        groceryList = [];
+    }
+    groceryList.push(value);
+    localStorage.setItem('groceryList', JSON.stringify(groceryList))
+}
+
+
+
+//! Display local storage
+
+function displayStorage() {
+    let exists = localStorage.getItem('groceryList');
+    
+    if(exists) {
+        let storageItems = JSON.parse(localStorage.getItem('groceryList'));
+        
+        storageItems.forEach(function(element){
+            createItem(element)
+        })
+    }
+}
+
+function clearAll(){
+//delete for local storage
+localStorage.removeItem('groceryList');
+
+let items = document.querySelectorAll('.grocery-item')
+if (items.length >0) {
+    showAction(displayItemsAction, 'All items deleted', false)
+    items.forEach(function (element) {
+        list.removeChild(element);
+    })
+}
+else showAction(displayItemsAction, 'No more items to delete', true)
 }
